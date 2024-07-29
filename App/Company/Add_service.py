a876a00e869c78b6d8573f043c1a7544b8d20816
FlamingoLindo <101421364+FlamingoLindo@users.noreply.github.com>
2024-07-29 14:58:37 -0300

    Add service

---
+++ App/Company/Add_service.py
@@ -0,0 +1,203 @@
+import unittest
+from appium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from appium.options.android import UiAutomator2Options
+from appium.webdriver.common.appiumby import AppiumBy
+import customtkinter as tk
+from tkinter import simpledialog
+import random
+from datetime import datetime 
+import time  
+import os
+import sys
+
+from dotenv import load_dotenv
+load_dotenv()
+
+# Add the path to the directory containing the Functions module
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from Functions.Rand_CPF import gera_e_valida_cpf
+
+def get_user_input(prompt):
+    root = tk.CTk()
+    root.withdraw()  # Hide the main window
+
+    user_input = simpledialog.askstring("Input", prompt)
+
+    return user_input
+
+def get_time():
+    date_time = datetime.today().strftime('%Y-%m-%d %H:%M:%S')
+    
+    return date_time
+
+screenshot_dir = r'Images\Screenshot'
+os.makedirs(screenshot_dir, exist_ok=True)
+screenshot_path = os.path.join(screenshot_dir, 'screenshot.png')
+
+
+capabilities = dict(
+    platformName='Android',
+    automationName='uiautomator2',
+    deviceName='flamingo_lindo',
+    language='pt',
+    printPageSourceOnFindFailure = True,
+    eventTimings = True,
+    noReset = True,
+    enableMultiWindows = True,
+    appPackage = 'com.mestresdaweb.motorfind',
+    appActivity = 'com.mestresdaweb.motorfind.MainActivity'
+)
+
+appium_server_url = 'http://localhost:4723'
+
+class TestAppium(unittest.TestCase):
+    def setUp(self) -> None:
+        self.driver = webdriver.Remote(appium_server_url, options=UiAutomator2Options().load_capabilities(capabilities))
+
+    def tearDown(self) -> None:
+        if self.driver:
+            self.driver.quit()
+
+    def test_edit_account(self) -> None:
+        count = 1
+        
+        try:
+            # Wait untill the app has loaded
+            WebDriverWait(self.driver, 20).until(
+                EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("com.horcrux.svg.PathView").instance(0)'))
+            )
+
+            # Inputs the email
+            email = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("insira seu e-mail")'))
+            ).send_keys(os.getenv("CLIENT_LOGIN"))
+
+            # Inputs the password
+            password = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Insira sua senha")'))
+            ).send_keys(os.getenv("CLIENT_PASSWORD"))
+            
+            # Button click
+            next_btn = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(1)'))
+            ).click()
+            
+            time.sleep(1)
+            
+            print("First step done 🟢")
+        except Exception as e:
+            self.driver.get_screenshot_as_file(screenshot_path)
+            print("There has been an error on the first step 🔴")
+            print(e)
+            raise  
+            
+        try:
+            # Open company menu
+            company_btn = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((AppiumBy.XPATH, '//android.widget.FrameLayout[@resource-id="android:id/content"]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.Button[1]'))
+            ).click()
+            
+            # Open company's products
+            my_products = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(3)'))
+            ).click()
+            
+            print("Second step done 🟢")
+        except Exception as e:
+            self.driver.get_screenshot_as_file(screenshot_path)
+            print("There has been an error on the second step 🔴")
+            print(e)
+            raise  
+            
+        try:
+            # Ask how many products are going to be made
+            service_amount_str = get_user_input("How many?")
+            service_amount_int = int(service_amount_str)
+            for _ in range(service_amount_int):
+                # Click the add service button
+                add_service = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Adicionar serviço")'))
+                ).click()
+                
+                # Input service name
+                service_name = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("nome do serviço oferecido")'))
+                ).send_keys(f"Auto service {count}" + get_time())
+                
+                # Generate random duration
+                rand_duration = random.randint(1, 99)
+                # Input service duration
+                service_name = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("00")'))
+                ).send_keys(rand_duration)
+                
+                # Time type dropdown
+                
+                # Subcategory dropdown
+                
+                # Generate random value
+                rand_value = random.randint(5, 9999)
+                # Input random value
+                service_value = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Escolha a subcategoria")'))
+                ).send_keys(rand_value)
+                
+                # Click the 'add photo' button
+                add_photo_btn = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().className("android.view.View").instance(1)'))
+                ).click()
+
+                # Click the 'album' button
+                albuns = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((AppiumBy.ACCESSIBILITY_ID, 'Álbuns'))
+                )
+                albuns.click()
+
+                # Open the favorites album
+                favorites = WebDriverWait(self.driver, 10).until(
+                            EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().resourceId("com.google.android.providers.media.module:id/icon_thumbnail").instance(0)'))
+                        )
+                favorites.click()
+                
+                # Click at the first image
+                image = WebDriverWait(self.driver, 10).until(
+                            EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().resourceId("com.google.android.providers.media.module:id/icon_thumbnail").instance(0)'))
+                        )
+                image.click()
+                
+                # Input service description
+                service_description = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Descreva o serviço oferecido")'))
+                ).send_keys(f"Auto service descriotion {count}" + get_time())
+                
+                # Input service link
+                service_link = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Descreva o serviço oferecido")'))
+                ).send_keys("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+                
+                # Click add service button
+                add_service_btn = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Adicionar serviço").instance(1)'))
+                ).click()
+                
+                # Close modal
+                continue_btn = WebDriverWait(self.driver, 10).until(
+                    EC.presence_of_element_located((AppiumBy.ANDROID_UIAUTOMATOR, 'new UiSelector().text("Continuar")'))
+                ).click()
+
+                count += 1
+            
+            print("Third step done 🟢")
+        except Exception as e:
+            self.driver.get_screenshot_as_file(screenshot_path)
+            print("There has been an error on the third step 🔴")
+            print(e)
+            raise  
+
+            
+                
+if __name__ == '__main__':
+    unittest.main()
