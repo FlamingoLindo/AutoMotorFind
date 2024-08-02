d766a584a2994c23f5d1357d5423f27fb14410df
FlamingoLindo <101421364+FlamingoLindo@users.noreply.github.com>
2024-08-02 15:54:47 -0300

    Comments

--- Client/Profile/Create_account.py
+++ Client/Profile/Create_account.py
@@ -45,98 +45,98 @@ account_amount_int = int(account_amount_str)
 
 for _ in range(account_amount_int):
     
-    # 
+    # Click the register button
     register_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]')
                                                     )
                         ).click()
     
-    # 
+    # Inputs name
     name_input = wait.until(EC.element_to_be_clickable((By.ID, 'name')
                                                     )
                         ).send_keys(create_random_name() +  f' Automatico (WEB) {count} ' + get_time())
     
-    # 
+    # Inputs email
     email_input = wait.until(EC.element_to_be_clickable((By.ID, 'email')
                                                     )
                         ).send_keys(create_random_name() + f'@gmail.com')
     
     # Generate random phone number (1)
     rand_phone = random.randint(11111111111, 99999999999)
-    # 
+    # Inputs phone number
     phone_input = wait.until(EC.element_to_be_clickable((By.ID, 'phone')
                                                     )
                         ).send_keys(rand_phone)
     
-    # 
+    # Click next button 1
     next_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.dCoGxG')
                                                     )
                         ).click()
     
-    # 
+    # Inputs random CPF
     cpf_input = wait.until(EC.element_to_be_clickable((By.ID, 'cpf')
                                                     )
                         ).send_keys(gera_e_valida_cpf())
     
-    # 
+    # Inputs random CPNJ
     cnpj_input = wait.until(EC.element_to_be_clickable((By.ID, 'cnpj')
                                                     )
                         ).send_keys(gera_cnpj())
     
     # Generate random phone number (1)
     rand_telephone = random.randint(11111111111, 99999999999)
-    # 
+    # Inputs random telephone number
     telephone_input = wait.until(EC.element_to_be_clickable((By.ID, 'telephone')
                                                     )
                         ).send_keys(rand_telephone)
 
-    # 
+    # Click next button 2
     next_btn2 = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.hvAiZb')
                                                     )
                         ).click()
     
-    # 
+    # Click the country dropdown
     country_dorpdown = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.jSZTir')
                                                     )
                         ).click()
     
-    #
+    # Serach Brazil
     country_search = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.eSxcE')
                                                     )
                         ).send_keys("Brasil")
     
-    # 
+    # Select Brazil
     country_select = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.gNYYxY')
                                                     )
                         ).click()
     
-    #
+    # Inputs CEP
     cep_input = wait.until(EC.element_to_be_clickable((By.ID, 'cep')
                                                     )
                         ).send_keys('01001-000')
     
-    #
+    # Inputs adress number
     number_input = wait.until(EC.element_to_be_clickable((By.ID, 'number')
                                                     )
                         ).send_keys(count)
 
     time.sleep(1.5)
     
-    # 
+    # Click next button 3
     next_btn3 = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.hNJTTa')
                                                     )
                         ).click()
     
-    #
+    # Inputs password
     password = wait.until(EC.element_to_be_clickable((By.NAME, 'password')
                                                     )
                         ).send_keys("Aa12345678!")
     
-    #
+    # Inputs password confirmatios
     password = wait.until(EC.element_to_be_clickable((By.NAME, 'confirm_password')
                                                     )
                         ).send_keys("Aa12345678!")
     
-    # 
+    # Click next button 4
     next_btn4 = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.jLEUkB')
                                                     )
                         ).click()
@@ -147,7 +147,7 @@ for _ in range(account_amount_int):
 
     for i in range(1, 9):
         
-        # Try using a different way to select checkboxes
+        # Selects all 8 interests
         interests = wait.until(EC.element_to_be_clickable(
             (By.XPATH, f'(//input[@type="checkbox"])[{count_inte}]')
         )).click()
@@ -155,40 +155,40 @@ for _ in range(account_amount_int):
         count_inte += 1
 
         
-    # 
+    # Click next button 5
     next_btn5 = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.hFGBKx')
                                                     )
                         ).click()
     
-    # 
+    # Agree to the terms and conditions
     terms = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.leEbPC')
                                                     )
                         ).click()
     
-    # 
+    # Click the continue button
     continue_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.jjwRFZ')
                                                     )
                         ).click()
     
-    # 
+    # Close modal
     modal = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.dNCRpz')
                                                     )
                         ).click()
-    # 
+    
+    # Click the account menu
     account_dropdown = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.bNVGjx')
                                                     )
                         ).click()
     
-    # 
+    # Exit account
     exit = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/span[8]')
                                                     )
                         ).click()
     
+    # Print the accout creation iteration
     print(f"Account {count} created")
     
     count +=1
-    
-    
     
 get_user_input("DONE")
 # Close the browser
