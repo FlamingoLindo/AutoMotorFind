9ba8b93d1bcfa102ab1e0359f0696fef491ce392
FlamingoLindo <101421364+FlamingoLindo@users.noreply.github.com>
2024-08-15 16:46:57 -0300

    try and except

--- Client/Profile/Add_card_bank.py
+++ Client/Profile/Add_card_bank.py
@@ -36,123 +36,76 @@ def get_user_input(prompt):
 
     return user_input
 
-# Drive stuff
-driver_path = './chromedriver.exe'
-s = Service(driver_path)
-driver = webdriver.Chrome(service=s)  
+try:
+    # Drive stuff
+    driver_path = './chromedriver.exe'
+    s = Service(driver_path)
+    driver = webdriver.Chrome(service=s)  
 
-driver.get(os.getenv('CLIENT_URL'))
+    driver.get(os.getenv('CLIENT_URL'))
 
-wait = WebDriverWait(driver, 5)
+    wait = WebDriverWait(driver, 5)
+except Exception as e:
+    print('There has been an error initialising the chrome driver')
+    print(e)
 
-# Click the login button
-login_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.hygPet')
-                                                    )
-                        ).click() 
+try:
+    # Click the login button
+    login_btn = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.hygPet')
+                                                        )
+                            ).click() 
 
-# Get login
-login = get_user_input('Email')
+    # Get login
+    login = get_user_input('Email')
 
-# Input login
-login_input = wait.until(EC.element_to_be_clickable((By.ID, 'email')
-                                                    )
-                        ).send_keys(login)
+    # Input login
+    login_input = wait.until(EC.element_to_be_clickable((By.ID, 'email')
+                                                        )
+                            ).send_keys(login)
 
-# Get password
-password = get_user_input('Password')
+    # Get password
+    password = get_user_input('Password')
 
-# Input password
-password_input = wait.until(EC.element_to_be_clickable((By.ID, 'password')
-                                                    )
-                        ).send_keys(password)
+    # Input password
+    password_input = wait.until(EC.element_to_be_clickable((By.ID, 'password')
+                                                        )
+                            ).send_keys(password)
 
-# Logs in 
-do_login = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.eaqkjo')
-                                                    )
-                        ).click() 
+    # Logs in 
+    do_login = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.eaqkjo')
+                                                        )
+                            ).click() 
+except Exception as e:
+    print('There has been an error on the login')
+    print(e)
 
-# Click options
-options = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.bNVGjx')
-                                                    )
-                        ).click() 
+try:
+    # Click options
+    options = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '.bNVGjx')
+                                                        )
+                            ).click() 
 
-# Click my profile option
-my_profile_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/span[4]')
-                                                    )
-                        ).click()
+    # Click my profile option
+    my_profile_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[1]/div[2]/div[1]/div[2]/div[2]/span[4]')
+                                                        )
+                            ).click()
 
-# Click wallet option
-wallet_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[1]/button[4]')
-                                                    )
-                        ).click()  
-
-"""# Click add bank account
-add_bank_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[1]/button')
-                                                    )
-                        ).click()  
-
-# Diced cpf or cnpj
-cpf_or_cnpj = random.randint(1,2)
-
-if cpf_or_cnpj == 1:
-    cpf_input = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="cpf_cnpj"]')
-                                                    )
-                        ).send_keys(gera_e_valida_cpf()) 
-else:
-    cnpj_input = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="cpf_cnpj"]')
-                                                    )
-                        ).send_keys(gera_cnpj()) 
+    # Click wallet option
+    wallet_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[1]/button[4]')
+                                                        )
+                            ).click()  
+except Exception as e:
+    print('There has been an error opening the profile options')
+    print(e)
     
-# Input account number
-acc_num = wait.until(EC.element_to_be_clickable((By.ID, 'account_number')
-                                                    )
-                        ).send_keys(ACC_NUM)
+"""
+try:
+    # Click add bank account
+    add_bank_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[1]/button')
+                                                        )
+                            ).click()  
 
-# Input agency
-agency = wait.until(EC.element_to_be_clickable((By.ID, 'agency')
-                                                    )
-                        ).send_keys(AGENCY) 
-
-# Click the account type dropdown
-acc_type_dropdown = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/form/div[4]/div/div')
-                                                    )
-                        ).click() 
-    
-# Account type decision
-acc_type_random = random.randint(1,2)
-
-acc_type_input = wait.until(EC.element_to_be_clickable((By.XPATH, f'/html/body/div[1]/form/div[4]/div/div[2]/div[{acc_type_random}]/span')
-                                                )
-                    ).click() 
-
-# Click bank type dropdown
-bank_type_dropdown = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/form/div[5]/div/div')
-                                                    )
-                        ).click() 
-
-# Bank type decision
-bank_type_random = random.randint(1,4)
-
-bank_type_input = wait.until(EC.element_to_be_clickable((By.XPATH, f'/html/body/div[1]/form/div[5]/div/div[2]/div[{bank_type_random}]')
-                                                  )
-                    ).click() 
-
-# Click the sabe button
-save_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/form/div[6]/button[1]')
-                                                  )
-                    ).click() """
-
-# Ask for the amount of cards to be created
-card_amount_str = get_user_input('Card amount?')
-card_amount_int = int(card_amount_str)
-for _ in range(card_amount_int):
-
-    # Click the add card button
-    add_card_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[2]/button')
-                                                    )
-                        ).click()
-
-    # CPF or CNPJ decision
+    # Diced cpf or cnpj
     cpf_or_cnpj = random.randint(1,2)
 
     if cpf_or_cnpj == 1:
@@ -162,33 +115,102 @@ for _ in range(card_amount_int):
     else:
         cnpj_input = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="cpf_cnpj"]')
                                                         )
-                            ).send_keys(gera_cnpj())
+                            ).send_keys(gera_cnpj()) 
         
-    # Input card name
-    name_input = wait.until(EC.element_to_be_clickable((By.ID, 'name')
-                                                    )
-                        ).send_keys('autonome')
+    # Input account number
+    acc_num = wait.until(EC.element_to_be_clickable((By.ID, 'account_number')
+                                                        )
+                            ).send_keys(ACC_NUM)
 
-    # Inpur card number
-    card_num = wait.until(EC.element_to_be_clickable((By.ID, 'card_number')
-                                                    )
-                        ).send_keys(CARD_NUM)
+    # Input agency
+    agency = wait.until(EC.element_to_be_clickable((By.ID, 'agency')
+                                                        )
+                            ).send_keys(AGENCY) 
 
-    # Input card expiration date
-    expi_date = wait.until(EC.element_to_be_clickable((By.ID, 'date')
-                                                    )
-                        ).send_keys(EXPI_DATE)
+    # Click the account type dropdown
+    acc_type_dropdown = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/form/div[4]/div/div')
+                                                        )
+                            ).click() 
+        
+    # Account type decision
+    acc_type_random = random.randint(1,2)
 
-    # Input card cvv
-    cvv = wait.until(EC.element_to_be_clickable((By.ID, 'cvv')
+    acc_type_input = wait.until(EC.element_to_be_clickable((By.XPATH, f'/html/body/div[1]/form/div[4]/div/div[2]/div[{acc_type_random}]/span')
                                                     )
-                        ).send_keys(CVV)
+                        ).click() 
 
-    # Click save button
-    save_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/form/div[5]/button[1]')
+    # Click bank type dropdown
+    bank_type_dropdown = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/form/div[5]/div/div')
+                                                        )
+                            ).click() 
+
+    # Bank type decision
+    bank_type_random = random.randint(1,4)
+
+    bank_type_input = wait.until(EC.element_to_be_clickable((By.XPATH, f'/html/body/div[1]/form/div[5]/div/div[2]/div[{bank_type_random}]')
                                                     )
-                        ).click()
+                        ).click() 
 
+    # Click the sabe button
+    save_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/form/div[6]/button[1]')
+                                                    )
+                        ).click() 
+except Exception as e:
+    print('There has been an error while creating the bank account')
+    print(e)
+"""
+try:
+    # Ask for the amount of cards to be created
+    card_amount_str = get_user_input('Card amount?')
+    card_amount_int = int(card_amount_str)
+    for _ in range(card_amount_int):
+
+        # Click the add card button
+        add_card_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/div[2]/div[2]/div[2]/button')
+                                                        )
+                            ).click()
+
+        # CPF or CNPJ decision
+        cpf_or_cnpj = random.randint(1,2)
+
+        if cpf_or_cnpj == 1:
+            cpf_input = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="cpf_cnpj"]')
+                                                            )
+                                ).send_keys(gera_e_valida_cpf()) 
+        else:
+            cnpj_input = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="cpf_cnpj"]')
+                                                            )
+                                ).send_keys(gera_cnpj())
+            
+        # Input card name
+        name_input = wait.until(EC.element_to_be_clickable((By.ID, 'name')
+                                                        )
+                            ).send_keys('autonome')
+
+        # Inpur card number
+        card_num = wait.until(EC.element_to_be_clickable((By.ID, 'card_number')
+                                                        )
+                            ).send_keys(CARD_NUM)
+
+        # Input card expiration date
+        expi_date = wait.until(EC.element_to_be_clickable((By.ID, 'date')
+                                                        )
+                            ).send_keys(EXPI_DATE)
+
+        # Input card cvv
+        cvv = wait.until(EC.element_to_be_clickable((By.ID, 'cvv')
+                                                        )
+                            ).send_keys(CVV)
+
+        # Click save button
+        save_btn = wait.until(EC.element_to_be_clickable((By.XPATH, '/html/body/div[1]/form/div[5]/button[1]')
+                                                        )
+                            ).click()
+except Exception as e:
+    print('There has been an error creating the credit card')
+    print(e)
+    
+    
 get_user_input("DONE")
 # Close the browser
 driver.quit()
