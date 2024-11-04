e8d5993bf4f4e7bf0ef0b3300dcdc881bad50e5c
FlamingoLindo <101421364+FlamingoLindo@users.noreply.github.com>
2024-11-04 12:52:25 -0300

    filters

---
+++ Filters/5Condition/new.py
@@ -0,0 +1,17 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 5)
+
+driver.get('https://motorfind-client.netlify.app/carhome/news')
+
+cars_page = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.gFOhXd')))
+
+new = driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/form/div[4]/div[1]')
+new.click()
+
+time.sleep(10)
---
+++ Filters/6Mileage/min_milage.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 5)
+
+driver.get('https://motorfind-client.netlify.app/carhome/news')
+
+cars_page = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.gFOhXd')))
+
+min_milage = driver.find_element(By.ID,'min_km')
+min_milage.send_keys(1)
+
+
+time.sleep(10)
